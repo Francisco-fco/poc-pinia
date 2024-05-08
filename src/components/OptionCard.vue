014b819385ff913ca0fbc9bf8a4ff6a9cf3577ce
Francisco Mouat <francisco.mouat@ldc.lu.se>
2024-05-08 10:16:17 +0200

    clean up option card

--- src/components/OptionCard.vue
+++ src/components/OptionCard.vue
@@ -1,10 +1,10 @@
 <template> 
-    <div class="col-md-4 col-6">  
-      <div class="card border border border-top-0" style="max-width: 20rem;">
-        <div class="card-header"><h4>{{header}}</h4></div>
-        <div class="card-body">
-          <h5 class="card-title">{{ title }}</h5>          
-          <p class="card-text">{{text}}</p>         
+    <div>  
+      <div style="max-width: 20rem;">
+        <div><h4>{{header}}</h4></div>
+        <div>
+          <h5>{{ title }}</h5>          
+          <p>{{text}}</p>         
           <router-link :to="route" class="btn btn-primary">{{btn}}</router-link>
         </div>
       </div>  
@@ -32,16 +32,9 @@
 </script>
 
 <style>
-  .card {
-    height: 100%;
-    
-  }
   .card-body {
-    display:flex;
     height: 100%;
     flex-direction: column;
   }
-  .card-text {
-    flex-grow: 1;
-  }
+  
 </style>
